614ba98bf82b3fedc78cc977ee16beaa8079ff43
jacksonyjh <jxcksxnpersonal@gmail.com>
2025-03-04 14:37:32 -0800

    used sql queries from hw2, recreated tables

--- project.py
+++ project.py
@@ -3,6 +3,116 @@ import sys
 import os
 
 
+# lowk might hard code these create table queries (source: homework 2 solutions)
+query_map = dict()
+query_map["users"] = """CREATE TABLE Users (
+    uid INT,
+    email TEXT NOT NULL,
+    joined_date DATE NOT NULL,
+    nickname TEXT NOT NULL,
+    street TEXT,
+    city TEXT,
+    state TEXT,
+    zip TEXT,
+    genres TEXT,
+    PRIMARY KEY (uid)
+);
+"""
+
+query_map["producers"] = """CREATE TABLE Producers (
+    uid INT,
+    bio TEXT,
+    company TEXT,
+    PRIMARY KEY (uid),
+    FOREIGN KEY (uid) REFERENCES Users(uid) ON DELETE CASCADE
+);
+"""
+
+
+query_map["viewers"] = """CREATE TABLE Viewers (
+    uid INT,
+    subscription ENUM('free', 'monthly', 'yearly'),
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    PRIMARY KEY (uid),
+    FOREIGN KEY (uid) REFERENCES Users(uid) ON DELETE CASCADE
+);
+"""
+query_map["releases"] = """CREATE TABLE Releases (
+    rid INT,
+    producer_uid INT NOT NULL,
+    title TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    release_date DATE NOT NULL,
+    PRIMARY KEY (rid),
+    FOREIGN KEY (producer_uid) REFERENCES Producers(uid) ON DELETE CASCADE
+);
+"""
+
+query_map["movies"] = """CREATE TABLE Movies (
+    rid INT,
+    website_url TEXT,
+    PRIMARY KEY (rid),
+    FOREIGN KEY (rid) REFERENCES Releases(rid) ON DELETE CASCADE
+);
+"""
+
+query_map["series"] = """CREATE TABLE Series (
+    rid INT,
+    introduction TEXT,
+    PRIMARY KEY (rid),
+    FOREIGN KEY (rid) REFERENCES Releases(rid) ON DELETE CASCADE
+);
+"""
+query_map["videos"] = """CREATE TABLE Videos (
+    rid INT,
+    ep_num INT NOT NULL,
+    title TEXT NOT NULL,
+    length INT NOT NULL,
+    PRIMARY KEY (rid, ep_num),
+    FOREIGN KEY (rid) REFERENCES Releases(rid) ON DELETE CASCADE
+);
+"""
+query_map["sessions"] = """CREATE TABLE Sessions (
+    sid INT,
+    uid INT NOT NULL,
+    rid INT NOT NULL,
+    ep_num INT NOT NULL,
+    initiate_at DATETIME NOT NULL,
+    leave_at DATETIME NOT NULL,
+    quality ENUM('480p', '720p', '1080p'),
+    device ENUM('mobile', 'desktop'),
+    PRIMARY KEY (sid),
+    FOREIGN KEY (uid) REFERENCES Viewers(uid) ON DELETE CASCADE,
+    FOREIGN KEY (rid, ep_num) REFERENCES Videos(rid, ep_num) ON DELETE CASCADE
+);
+"""
+
+query_map["reviews"] = """CREATE TABLE Reviews (
+    rvid INT,
+    uid INT NOT NULL,
+    rid INT NOT NULL,
+    rating DECIMAL(2, 1) NOT NULL CHECK (rating BETWEEN 0 AND 5),
+    body TEXT,
+    posted_at DATETIME NOT NULL,
+    PRIMARY KEY (rvid),
+    FOREIGN KEY (uid) REFERENCES Viewers(uid) ON DELETE CASCADE,
+    FOREIGN KEY (rid) REFERENCES Releases(rid) ON DELETE CASCADE
+);
+"""
+
+
+table_order = [
+        "users",
+        "producers",
+        "viewers",
+        "releases",
+        "movies",
+        "series",
+        "videos",
+        "sessions",
+        "reviews"
+    ]
 # db = mysql.connector.connect(
 #     host = "localhost",
 #     user = "test",
@@ -76,16 +186,11 @@ def main():
     if function_name == "import":
         folder_name = args[0]
         load_data(folder_name)
-    elif function_name == "insertViewer":
-        insert_viewer(*args)
+    # elif function_name == "insertViewer":
+    #     insert_viewer(*args)
     # Add other commands similarly
     else:
         print("Unknown function.")
-
-def create_table_query(cols table_name,):
-    """Takes in string list columns and tablename, returns query to create table for DB"""
-    id = cols[0] # first index always id
-    query  = f"CREATE TABLE {table_name} ("
 
 
 
@@ -110,27 +215,39 @@ def load_data(folder_name):
     #     )
     # """)
 
+    for table_name in table_order:
+        cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+        cursor.execute(query_map[table_name])
+
+        print(f"Created {table_name} table")
+
+
     # Load CSV files into tables
-    for filename in os.listdir(folder_name):
-        table_name = filename.split('.')[0]
-        print("TABLE NAME: ", table_name)
-        file_path = os.path.join(folder_name, filename)
-        print(f"Loading data from {file_path} into table {table_name}.")
-        with open(file_path, 'r') as file:
-            column_names = file.readline().strip("\n").split(",")
-            print("Columns: ", column_names)
+    # for filename in os.listdir(folder_name):
+    #     table_name = filename.split('.')[0]
+    #     print("TABLE NAME: ", table_name)
+    #     file_path = os.path.join(folder_name, filename)
+    #     print(f"Loading data from {file_path} into table {table_name}.")
+    #     with open(file_path, 'r') as file:
 
-            create_table(column_names, table_name)
+    #         # Delete already existing tables
+    #         # cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
 
-            # cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join([f'{col} VARCHAR(255)' for col in column_names])});")
+    #         column_names = file.readline().strip("\n").split(",")
+    #         print("Columns: ", column_names)
 
-            print("CREATED TABLE: ", table_name)
-            for line in file:
-                values = line.strip().split(',')
-                print(values)
-                # Use prepared statements for insertion
-                query = f"INSERT INTO {table_name} VALUES ({','.join(['%s'] * len(values))})"
-                cursor.execute(query, values)
+    #         # Create tables
+    #         cursor.execute(query_map[table_name])
+
+    #         # cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join([f'{col} VARCHAR(255)' for col in column_names])});")
+
+    #         # print("CREATED TABLE: ", table_name)
+    #         # for line in file:
+    #         #     values = line.strip().split(',')
+    #         #     print(values)
+    #         #     # Use prepared statements for insertion
+    #         #     query = f"INSERT INTO {table_name} VALUES ({','.join(['%s'] * len(values))})"
+    #         #     cursor.execute(query, values)
 
     # connection.commit()
     print("Data import successful.")
