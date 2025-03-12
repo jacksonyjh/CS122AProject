758edc308afe34e03e6b7151f13b81c7a9d80bc9
Novyanna Tsang <nntsang@uci.edu>
2025-03-12 16:16:18 -0700

    popular releases

--- selects.py
+++ selects.py
@@ -30,3 +30,34 @@ def select_releases(uid):
     cursor.close()
     connection.close()
     return
+
+
+def popular_releases(num):
+    connection = db_utils.connect_to_cs122a()
+    if not connection:
+        print("Failed to connect to cs122a database.")
+        return
+
+    cursor = connection.cursor()
+
+    pop_releases_query = """
+    SELECT releases.rid, releases.title, COUNT(reviews.rvid) AS reviewCount
+    FROM releases
+    JOIN reviews ON reviews.rid = releases.rid
+    GROUP BY releases.rid
+    ORDER BY reviewCount DESC, releases.rid;
+    LIMIT %s
+"""
+
+    cursor.execute(pop_releases_query, (num,))
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    print(f"top {num} releases selected!")
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return
